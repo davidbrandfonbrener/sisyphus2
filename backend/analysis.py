e436ee870aeaec1ff85d84371da250d0c7f23ca0
David Brandfonbrener <david.brandfonbrener@yale.edu>
2017-09-26 11:57:15 -0400

    updated analysis to clean up visualization

--- backend/analysis.py
+++ backend/analysis.py
@@ -7,9 +7,29 @@ from sklearn.decomposition import PCA
 import matplotlib.animation
 
 
-def update_plot(i, data, scat):
-    scat.set_array(data[i])
-    return scat,
+def plot_states(states, I):
+    pca = PCA(n_components=4)
+    pca.fit(states[:, 0, :])
+    reduced = pca.transform(states[:, 0, :])[:, :2]
+
+    I_pca = pca.transform(I)
+
+    fig, ax = plt.subplots()
+    a, b = [I_pca[0,0]], [I_pca[0,1]]
+    sc = ax.scatter(a, b)
+    plt.xlim(1.1 * min(reduced[:, 0]), 1.1 * max(reduced[:, 0]))
+    plt.ylim(1.1 * min(reduced[:, 1]), 1.1 * max(reduced[:, 1]))
+
+    def animate(i):
+        a.append(reduced[i, 0])
+        b.append(reduced[i, 1])
+        sc.set_offsets(np.c_[a, b])
+        sc.set_color(['r'] + [str(.99 * float(x) / len(reduced[:, 0])) for x in range(len(a))])
+
+    ani = matplotlib.animation.FuncAnimation(fig, animate,
+                                             frames=len(reduced[:, 0]), interval=100, repeat=False)
+    plt.show()
+    return
 
 # input: simulator object, params dict
 # output: fixed points found by hahnloser strategy
@@ -17,26 +37,6 @@ def update_plot(i, data, scat):
 def hahnloser_fixed_point(sim, trial):
 
     outputs, states = sim.run_trial(trial)
-
-    pca = PCA(n_components=2)
-    pca.fit(states[:,0,:])
-    reduced = pca.transform(states[:,0,:])
-
-    fig, ax = plt.subplots()
-    a, b = [], []
-    sc = ax.scatter(a, b)
-    plt.xlim(min(reduced[:,0]), max(reduced[:,0]))
-    plt.ylim(min(reduced[:,1]), max(reduced[:,1]))
-
-    def animate(i):
-        a.append(reduced[i,0])
-        b.append(reduced[i,1])
-        sc.set_offsets(np.c_[a, b])
-        sc.set_color([str(float(x) / len(reduced[:,0])) for x in range(len(a))])
-
-    ani = matplotlib.animation.FuncAnimation(fig, animate,
-                                             frames=len(reduced[:,0]), interval=100, repeat=True)
-    plt.show()
 
     #TODO decide what input to use
     input_vec = np.matmul(np.absolute(sim.W_in), np.array([.1]*sim.N_in))
@@ -65,7 +65,8 @@ def hahnloser_fixed_point(sim, trial):
 
         if fixed == True:
             # here I is the fixed point, while s is the attained state with the correct sign permutation
-            print trial[i, :], I, s, pca.transform(I)
+            print trial[i, :] #, I, s, pca.transform(I)
+            points.append(I)
 
     return points
 
@@ -80,10 +81,14 @@ if __name__ == '__main__':
     x,y,mask = flip_flop.build_train_batch(params)
 
     trial = x[0]
-    plt.ylim(-.1, 1.1)
-    plt.plot(trial)
-    plt.show()
+    #plt.ylim(-.1, 1.1)
+    #plt.plot(trial)
+    #plt.show()
 
     sim = Simulator(params, weights_path="../tasks/weights/flipflop.npz")
 
-    hahnloser_fixed_point(sim, trial)
+    points = hahnloser_fixed_point(sim, trial)
+
+    outputs, states = sim.run_trial(trial)
+
+    plot_states(states=states, I=points)
