97cc2a775a1fa6c1433c6befa2924791fd4a376d
AlexAtanasov <alexander.atanasov@yale.edu>
2017-10-11 01:57:40 -0400

    added setup.py, added 3 neruon network analysis notebook

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Sisyphus',
+    version='0.1dev',
+    packages=['tasks','backend',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
