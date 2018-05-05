c31f3e6bf4f11e52fd14730c64e7190c6ddbde9d
David Brandfonbrener <david.brandfonbrener@yale.edu>
2018-05-05 19:34:20 -0400

    added new object oriented task backend with RDM task

--- backend/task.py
+++ backend/task.py
@@ -1,46 +1,93 @@
 import numpy as np
 
 
-class Task:
-    def __init__(self, task_fun, params):
+class Task(object):
+    def __init__(self, N_in, N_out, dt, tau, T, N_batch):
 
-        self.task_fun = task_fun
-        self.dt = params['dt']
-        self.tau = params['tau']
+        self.N_batch = N_batch
+        self.N_in = N_in
+        self.N_out = N_out
+        self.dt = dt
+        self.tau = tau
+        self.T = T
+
         self.alpha = self.dt / self.tau
-        self.T = params['T']
         self.N_steps = int(np.ceil(self.T / self.dt))
 
-        self.N_in = params['N_in']
-        self.N_out = params['N_out']
+    # return params for a given batch
+    def trial_params_function(self, batch, trial):
+            pass
 
-        self.stim_noise = params['stim_noise']
+    # return input, output, mask, at time t
+    def trial_function(self, time, params):
+        pass
 
-
-    def generate_trial(self, params):
+    def __generate_trial__(self, params):
 
         x_data = np.zeros([self.N_steps, self.N_in])
         y_data = np.zeros([self.N_steps, self.N_out])
         mask = np.zeros([self.N_steps, self.N_out])
 
         for t in range(self.N_steps):
-            x_data[t, :], y_data[t, :], mask[t, :] = self.task_fun(t * self.dt, params)
-
-        x_data += np.sqrt(2 * self.alpha * self.stim_noise * self.stim_noise) * np.random.randn(self.N_steps, self.N_in)
+            x_data[t, :], y_data[t, :], mask[t, :] = self.trial_function(t * self.dt, params)
 
         return x_data, y_data, mask
 
+    def batch_generator(self):
+        batch = 1
+        while batch > 0:
+
+            x_data = []
+            y_data = []
+            mask = []
+            for trial in range(self.N_batch):
+                x,y,m = self.__generate_trial__(self.trial_params_function(batch, trial))
+                x_data.append(x)
+                y_data.append(y)
+                mask.append(m)
+
+            batch += 1
+
+            yield np.array(x_data), np.array(y_data), np.array(mask)
 
 
-def generate_batch(list_of_tasks):
+class RDM(Task):
 
-    while 1 > 0:
+    # return params for a given batch
+    def trial_params_function(self, batch, trial):
 
-        x_data = []
-        y_data = []
-        mask = []
+        params = dict()
+        params['coherence'] = np.random.choice([-0.3, -0.2, -0.1, 0.1, 0.2, 0.3])
+        params['stim_noise'] = 0.1
+        params['onset_time'] = np.random.random() * self.T / 2.0
+        params['stim_duration'] = np.random.random() * self.T / 4.0 + self.T / 8.0
 
-        for task in list_of_tasks:
-            x,y,m = task.generate_trial()
+        return params
 
-        yield
+    # return input, output, mask, at time t
+    def trial_function(self, time, params):
+
+        x_t = np.zeros(self.N_in) + np.sqrt(2*self.alpha*params['stim_noise']*
+                                        params['stim_noise'])*np.random.randn(self.N_in)
+        y_t = .1 * np.ones(self.N_out)
+
+
+        coh = params['coherence']
+        onset = params['onset_time']
+        stim_dur = params['stim_duration']
+
+
+
+        if onset < time < onset + stim_dur:
+            x_t[0] += coh
+            x_t[1] += -coh
+
+        if time > onset + stim_dur:
+            y_t[int(coh < 0)] = 1.
+
+        mask_t = np.ones(self.N_out)
+        if time < onset + stim_dur:
+            mask_t = np.zeros(self.N_out)
+
+        return x_t, y_t, mask_t
+
